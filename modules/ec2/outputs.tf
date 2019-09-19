0ceee9ded965253c216d0a37a25b7499ca33db5c
Srinivasarao Parvathaneni <sparvathaneni@innominds.com>
2019-09-19 16:23:15 +0530

    added igw and rt

---
+++ modules/ec2/outputs.tf
@@ -0,0 +1,9 @@
+output "ec2_instance_id" {
+  value = "aws_instance.web.id"
+}
+output "ec2_public_ip" {
+  value = "aws_instance.web.public_ip"
+}
+output "ec2_private_ip" {
+  value = "aws_instance.web.private_ip"
+}
--- modules/vpc/networking-vpc.tf
+++ modules/vpc/networking-vpc.tf
@@ -16,7 +16,7 @@ resource "aws_subnet" "main" {
   }
 }
 
-resource "aws_internet_gateway" "gw" {
+resource "aws_internet_gateway" "igw" {
   vpc_id = "${aws_vpc.main.id}"
 
   tags = {
@@ -25,10 +25,29 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
+resource "aws_route_table" "rt" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.igw.id}"
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-IGW"
+  }
+}
 output "r_vpc_id" {
   value = "${aws_vpc.main.id}"
 }
 
 output "r_subnet_id" {
   value = "${aws_subnet.main.id}"
+}
+
+output "r_igw_id" {
+  value = "${aws_internet_gateway.igw.id}"
+}
+output "r_route_table_id" {
+  value = "${aws_route_table.rt.id}"
 }
